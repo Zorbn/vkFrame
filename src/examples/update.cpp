5226ee9f897985cdb2d6e92a5f85584fc6ccb35a
Zorbn <public.zorb@gmail.com>
2022-12-03 13:25:07 -0800

    Fixed formatting in update example

--- src/examples/update.cpp
+++ src/examples/update.cpp
@@ -125,7 +125,8 @@ class App {
         textureSampler =
             textureImage.createTextureSampler(vulkanState.physicalDevice, vulkanState.device);
 
-        updateTestModel = Model<VertexData, uint16_t, InstanceData>::create(3, vulkanState.allocator, vulkanState.commands, vulkanState.graphicsQueue, vulkanState.device);
+        updateTestModel = Model<VertexData, uint16_t, InstanceData>::create(3, vulkanState.allocator,
+            vulkanState.commands, vulkanState.graphicsQueue, vulkanState.device);
         std::vector<InstanceData> instances = {InstanceData{glm::vec3(1.0f, 0.0f, 0.0f)},
                                                InstanceData{glm::vec3(0.0f, 1.0f, 0.0f)},
                                                InstanceData{glm::vec3(0.0f, 0.0f, 1.0f)}};
